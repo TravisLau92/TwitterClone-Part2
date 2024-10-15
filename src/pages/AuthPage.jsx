4444b7c7ce2080f89059da24dcaed1eb6039fcf1
Travis Lau <travislau92@hotmail.com>
2024-10-15 23:00:41 +0800

    Update AuthPage.jsx

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,16 +1,15 @@
-import { Col, Image, Row, Button, Modal, Form } from "react-bootstrap";
+import { Button, Col, Image, Row, Modal, Form } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import useLocalStorage from "use-local-storage";
 import { useNavigate } from "react-router-dom";
 
 export default function AuthPage() {
-  const loginImage = 'src/assets/twitter.png'
-  const url = 
-  "https://auth-back-end-sigmaschooltech.sigma-school-full-stack.repl.co";
+  const loginImage = "https://sig1.co/img-twitter-1";
+  const url =
+    "https://auth-back-end-sigmaschooltech.sigma-school-full-stack.repl.co";
 
   // Possible values: null (no modal shows), "Login", "SignUp"
-  
   const [modalShow, setModalShow] = useState(null);
   const handleShowSignUp = () => setModalShow("SignUp");
   const handleShowLogin = () => setModalShow("Login");
@@ -26,6 +25,15 @@ export default function AuthPage() {
     }
   }, [authToken, navigate]);
 
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${url}/signup`, { username, password });
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
@@ -34,7 +42,6 @@ export default function AuthPage() {
         setAuthToken(res.data.token); // Save token to localStorage.
         console.log("Login was successful, token saved");
       }
-      
     } catch (error) {
       console.error(error);
     }
@@ -43,78 +50,95 @@ export default function AuthPage() {
   return (
     <Row>
       <Col sm={6}>
-        <Image src={loginImage} fluid/>
+        <Image src={loginImage} fluid />
+      </Col>
+      <Col sm={6} className="p-4">
+        <i
+          className="bi bi-twitter"
+          style={{ fontSize: 50, color: "dodgerblue" }}
+        ></i>
+
+        <p className="mt-5" style={{ fontSize: 64 }}>
+          Happening Now
+        </p>
+        <h2 className="my-5" style={{ fontSize: 31 }}>
+          Join Twitter today.
+        </h2>
+        <Col sm={5} className="d-grid gap-2">
+          <Button className="rounded-pill" variant="outline-dark">
+            <i className="bi bi-google"></i> Sign up with Google
+          </Button>
+          <Button className="rounded-pill" variant="outline-dark">
+            <i className="bi bi-apple"></i> Sign up with Apple
+          </Button>
+          <p style={{ textAlign: "center" }}>or</p>
+          <Button className="rounded-pill" onClick={handleShowSignUp}>
+            Create an account
+          </Button>
+          <p style={{ fontSize: "12px" }}>
+            By signing up, you agree to the Terms of Service and Privacy Policy,
+            including Cookie Use.
+          </p>
+
+          <p className="mt-5" style={{ fontWeight: "bold" }}>
+            Already have an account?
+          </p>
+          <Button
+            className="rounded-pill"
+            variant="outline-primary"
+            onClick={handleShowLogin}
+          >
+            Sign in
+          </Button>
         </Col>
-        <Col sm={6} className="p-4">
-          <i className="bi bi-twitter" style={{ fontSize: 50, color: "dodgerblue" }}></i>
+        <Modal
+          show={modalShow !== null}
+          onHide={handleClose}
+          animation={false}
+          centered
+        >
+          <Modal.Body>
+            <h2 className="mb-4" style={{ fontWeight: "bold" }}>
+              {modalShow === "SignUp"
+                ? "Create your account"
+                : "Log in to your account"}
+            </h2>
+            <Form
+              className="d-grid gap-2 px-5"
+              onSubmit={modalShow === "SignUp" ? handleSignUp : handleLogin}
+            >
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control
+                  onChange={(e) => setUsername(e.target.value)}
+                  type="email"
+                  placeholder="Enter username"
+                />
+              </Form.Group>
 
-          <p className="mt-5" style={{ fontSize: 64 }}>Happening Now</p>
-          <h2 className="my-5" style={{ fontSize: 31 }}>Join Twitter Today.</h2>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Group>
+              <p style={{ fontSize: "12px" }}>
+                By signing up, you agree to the Terms of Service and Privacy
+                Policy, including Cookie Use. SigmaTweets may use your contact
+                information, including your email address and phone number for
+                purposes outlined in our Privacy Policy, like keeping your
+                account secure and personalising our services, including ads.
+                Learn more. Others will be able to find you by email or phone
+                number, when provided, unless you choose otherwise here.
+              </p>
 
-          <Col sm={5} className="d-grid gap-2">
-            <Button className="rounded-pill" variant="outline-dark">
-              <i className="bi bi-google"></i> Sign up with Google
+              <Button className="rounded-pill" type="submit">
+                {modalShow === "SignUp" ? "Sign up" : "Log in"}
               </Button>
-              <Button className="rounded-pill" variant="outline-dark">
-                <i className="bi bi-apple"></i> Sign up with Apple
-                </Button>
-                <p style={{ textAlign: "center" }}>or</p>
-               
-                <Button classname="rounded-pill" onClick={handleShowSignUp}>Create an account</Button>
-                <p style={{ fontSize: "12px" }}>
-                  By signing up, you agree to the Terms of Service and Privacy Policy including Cookie Use</p>
-
-                  <p className="mt-5" style={{ fontWeight: "bold"}}>
-                    Already have an account?
-                    </p>
-                    <Button 
-                    className="rounded-pill" 
-                    variant="outline-primary"
-                    onClick={handleShowLogin}
-                    >Sign In</Button>
-                    </Col>
-
-                    <Modal 
-                    show={modalShow !== null} 
-                    onHide={handleClose} 
-                    animation={false}
-                    centered>
-                      <Modal.Body>
-                        <h2 className="mb-4" style= {{ fontWeight: "bold"}}>
-                          {modalShow === "SignUp"
-                           ? "Create your account"
-                           : "Log in to your account"}
-                          </h2>
-                          <Form 
-                          className="d-grid gap-2 px-5" 
-                          obSubmit={modalShow === "SignUp" ? handleSignUp : handleLogin}
-                          >
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                              <Form.Control 
-                              onChange={(e) => setUsername(e.target.value)}
-                              type="email" 
-                              placeholder="Enter username" />
-                              </Form.Group>
-
-                              <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Control 
-                                onChange={(e) => setPassword(e.target.value)}
-                                type="password" 
-                                placeholder="Password" />
-                                </Form.Group>
-                                
-                                <p style={{ fontSize: "12px" }}>
-                                  By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use. SigmaTweets may use your contact information, including your email address and phone number for purposes outlined in our Privary Policy, like keeping your account secure and personalising our services, including ads. Learn more. Others will be able to find you by email or phone number, when provided, unless you choose otherwise here. 
-                                  </p>
-
-                                  <Button className="rounded-pill" type="submit">
-                                    {modalShow === "SignUp" ? "Sign up" : "Login" }
-                                    </Button>
-                                  </Form>
-                                  </Modal.Body>
-                                  </Modal>
-                      
-        </Col>
-        </Row>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </Col>
+    </Row>
   );
 }
